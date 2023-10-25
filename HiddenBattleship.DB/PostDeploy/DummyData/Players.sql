47ea2397f36380c29a325b67ef7028e82fc8f857
Rafael Parra <rafael12@gmx.com>
2023-10-25 02:47:16 -0500

    added utPlayer tests

--- HiddenBattleship.DB/PostDeploy/DummyData/Players.sql
+++ HiddenBattleship.DB/PostDeploy/DummyData/Players.sql
@@ -1,9 +1,9 @@
 ﻿BEGIN 
 INSERT INTO tblPlayer (Id, UserName, Email, Password ) 
 VALUES 
-(NEWID(), 'kevin', 'smith', 'yes' ),
-(NEWID(), 'kevin', 'smith', 'yes' ),
-(NEWID(), 'kevin', 'smith', 'yes' ),
-(NEWID(), 'kevin', 'smith', 'yes' ),
-(NEWID(), 'kevin', 'smith', 'yes' )
+(NEWID(), 'user1', '123@gmail.com', '1234!' ),
+(NEWID(), 'user2', '456@gmail.com', '123!' ),
+(NEWID(), 'user3', '789@gmail.com', '12!' ),
+(NEWID(), 'user4', '123@yahoo.com', '1!' ),
+(NEWID(), 'user5', '456@yahoo.com', '1' )
 END
--- HiddenBattleship.PL.Tests/utPlayer.cs
+++ HiddenBattleship.PL.Tests/utPlayer.cs
@@ -1,13 +1,51 @@
 ﻿namespace HiddenBattleship.PL.Tests
 {
     [TestClass]
-
     public class utPlayer : utBase
     {
         [TestMethod]
         public void InsertTest()
         {
+            int results = 0;
+            tblPlayer tbPlayer = new tblPlayer();
+            tbPlayer.Email = "test";
+            tbPlayer.UserName = "test";
+            tbPlayer.Password = "test";
 
+
+            db.tblPlayers.Add(tbPlayer);
+            results = db.SaveChanges();
+
+            Assert.IsTrue(results > 0);
+        }
+
+        [TestMethod]
+        public void UpdateTest()
+        {
+            int result;
+            var row = db.tblPlayers.FirstOrDefault(p => p.UserName == "user1");
+            row.UserName = "afafafaf";
+
+            result = db.SaveChanges();
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void DeleteTest()
+        {
+            int result;
+            tblPlayer row = db.tblPlayers.FirstOrDefault(p => p.UserName == "user2");
+
+            db.tblPlayers.Remove(row);
+            result = db.SaveChanges();
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void LoadTest()
+        {
+            tblPlayer row = db.tblPlayers.FirstOrDefault(p => p.Email == "789@gmail.com");
+            Assert.AreEqual("12!", row.Password);
         }
     }
 }

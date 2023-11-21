18c5513f4382221bf4596213afa506552c99133d
Kathy Xiong <700201382@fvtc.edu>
2023-11-21 04:12:03 -0600

    Updated Player BL Test

--- HiddenBattleship.BL.Test/utPlayer.cs
+++ HiddenBattleship.BL.Test/utPlayer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HiddenBattleship.BL.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,31 +13,50 @@ namespace HiddenBattleship.BL.Test
         [TestMethod]
         public void LoadTest()
         {
-            
-        }
-
-        [TestMethod]
-        public void LoadById()
-        {
-
+            List<Player> players = new PlayerManager(options).Load();
+            Assert.IsTrue(players.Count > 0);
         }
 
         [TestMethod]
         public void InsertTest()
         {
+            Player player = new Player
+            {
+                UserName = "Test",
+                Password = "Test2",
+                Email = "Test3"
+            };
 
+            int result = new PlayerManager(options).Insert(player, true);
+            Assert.IsTrue(result > 0);
         }
 
         [TestMethod]
-        public void UpdateTest()
+        public void LoginSuccess()
         {
-
+            Player player = new Player { UserName = "Master", Password = "Test123", Email = "Key" };
+            bool result = new PlayerManager(options).Login(player);
+            Assert.IsTrue(result);
         }
 
         [TestMethod]
-        public void DeleteTest()
+        public void LoginFailure()
         {
+            try
+            {
+                Player player = new Player { UserName = "Master", Password = "xxxxx", Email = "Key" };
+                new PlayerManager(options).Login(player);
+                Assert.Fail();
+            }
+            catch (LoginFailureException)
+            {
+                Assert.IsTrue(true);
+            }
+            catch (Exception)
+            {
 
+                Assert.Fail();
+            }
         }
 
     }

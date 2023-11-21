616b7b8db0000b086376ad55d5b557df24dc25af
Kathy Xiong <700201382@fvtc.edu>
2023-11-21 03:32:42 -0600

    Updated ChatHistory bl test

--- HiddenBattleship.BL.Test/utChatHistory.cs
+++ HiddenBattleship.BL.Test/utChatHistory.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HiddenBattleship.BL.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,31 +13,49 @@ namespace HiddenBattleship.BL.Test
         [TestMethod]
         public void LoadTest()
         {
-
+            List<ChatHistory> chatHistories = new ChatHistoryManager(options).Load();
+            int expected = 3;
+            
+            Assert.AreEqual(expected, chatHistories.Count);
         }
 
         [TestMethod]
         public void LoadById()
         {
-
+            ChatHistory chatHistory = new ChatHistoryManager(options).Load().FirstOrDefault();
+            Assert.AreEqual(new ChatHistoryManager(options).LoadById(chatHistory.Id).Id, chatHistory.Id);
         }
 
         [TestMethod]
         public void InsertTest()
         {
+            ChatHistory chatHistory = new ChatHistory
+            {
+                Sender = System.Guid.NewGuid(),
+                Receiver = System.Guid.NewGuid(),
+                GameId = new GameManager(options).FirsorDefault().Id,
+                Timestamp = DateTime.Now
+            };
 
+            int result = new ChatHistoryManager(options).Insert(chatHistory, true);
+            Assert.IsTrue(result > 0);
         }
 
         [TestMethod]
         public void UpdateTest()
         {
+            ChatHistory chatHistory = new ChatHistoryManager(options).Load().FirstOrDefault();
+            chatHistory.Message = "Test";
 
+            Assert.IsTrue(new ChatHistoryManager(options).Update(chatHistory, true) > 0);
         }
 
         [TestMethod]
         public void DeleteTest()
         {
-
+            ChatHistory chatHistory = new ChatHistoryManager(options).Load().FirstOrDefault();
+            
+            Assert.IsTrue(new ChatHistoryManager(options).Delete(chatHistory.Id, true) > 0);
         }
     }
 }

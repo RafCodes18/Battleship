f2bc2fb3fb599f1c52683c6f05a7e560877522ee
Rafael Parra <rafael12@gmx.com>
2023-11-25 13:19:01 -0600

    Added basic profile page and view model

---
+++ HiddenBattleship.MVC.UI/ViewModels/ProfileVM.cs
@@ -0,0 +1,10 @@
+﻿using HiddenBattleship.BL.Models;
+
+namespace HiddenBattleship.MVC.UI.ViewModels
+{
+    public class ProfileVM
+    {
+        public Player player { get; set; }
+        public Game game { get; set; }
+    }
+}

078159a353de251b7336910e93e1c5e300b3bdf4
Kathy Xiong <700201382@fvtc.edu>
2023-12-05 00:29:30 -0600

    Had to redo WPF because of naming issues.. ;-;

---
+++ HiddenBattleShip.WPFUI/App.xaml.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace HiddenBattleShip.WPFUI
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        ServiceProvider serviceProvider;
+        public static IConfiguration Configuration;
+
+        public App()
+        {
+            var services = new ServiceCollection();
+            ConfigureServices(services);
+            serviceProvider = services.BuildServiceProvider();
+
+        }
+
+        private void ConfigureServices(ServiceCollection services)
+        {
+            var configSettings = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+            Configuration = configSettings;
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configSettings)
+                .CreateLogger();
+
+
+
+            services.AddSingleton<GameList>()
+                .AddLogging(c => c.AddConsole())
+                .AddLogging(c => c.AddDebug())
+                .AddLogging(c => c.AddSerilog())
+               .AddLogging(c => c.AddEventLog());
+
+            /*  services.AddSingleton<QRCode>()
+                 .AddLogging(c => c.AddConsole())
+                 .AddLogging(c => c.AddDebug())
+                 .AddLogging(c => c.AddSerilog())
+                 .AddLogging(c => c.AddEventLog()); */
+
+
+        }
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            var GameList = serviceProvider.GetService<GameList>();
+
+            //var screen = serviceProvider.GetService<QRCode>();
+
+            GameList.Show();
+        }
+
+
+    }
+
+}
---
+++ HiddenBattleShip.WPFUI/GameList.xaml.cs
@@ -0,0 +1,63 @@
+﻿using HiddenBattleship.BL.Models;
+using Microsoft.Build.Framework;
+using Microsoft.Extensions.Logging;
+using PB.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HiddenBattleShip.WPFUI
+{
+    /// <summary>
+    /// Interaction logic for GameList.xaml
+    /// </summary>
+    public partial class GameList : Window
+    {
+        List<Game> games = new List<Game>();
+        string APIAddress = "https://localhost:7270/swagger/v1/swagger.json";
+        ApiClient apiClient;
+        private readonly ILogger<GameList> logger;
+
+        public GameList()
+        {
+            InitializeComponent();
+            apiClient = new ApiClient(APIAddress);
+        }
+
+        public GameList(ILogger<GameList> logger)
+        {
+            InitializeComponent();
+            apiClient = new ApiClient(APIAddress);
+            this.logger = logger;
+        }
+
+        private void btnGetGames_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+
+                games = apiClient.GetList<Game>(typeof(Game).Name);
+
+                dgGames.ItemsSource = null;
+                dgGames.ItemsSource = games;
+
+                throw new Exception("Dependency Injection is cool. I have " + games.Count + " movies.");
+
+            }
+            catch (Exception ex)
+            {
+                logger.LogWarning("Error: {UserId}", " ");
+            }
+        }
+    }
+}

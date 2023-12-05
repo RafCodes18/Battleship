1e713564392f02b95827a27bfa09fe5878f336ca
Kathy Xiong <700201382@fvtc.edu>
2023-12-05 00:08:00 -0600

    Added GameList.xaml

--- HiddenBattleShipWPFUI/App.xaml.cs
+++ HiddenBattleShipWPFUI/App.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Configuration;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using System.Configuration;
 using System.Data;
 using System.Windows;
 
@@ -9,6 +13,55 @@ namespace HiddenBattleShip.WPFUI
     /// </summary>
     public partial class App : Application
     {
+        ServiceProvider serviceProvider;
+        public static IConfiguration Configuration;
+
+        public App()
+        {
+            var services = new ServiceCollection();
+            ConfigureServices(services);
+            serviceProvider = services.BuildServiceProvider();
+
+        }
+
+        private void ConfigureServices(ServiceCollection services)
+        {
+            var configSettings = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+            Configuration = configSettings;
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configSettings)
+                .CreateLogger();
+
+
+
+            services.AddSingleton<GameList>()
+                .AddLogging(c => c.AddConsole())
+                .AddLogging(c => c.AddDebug())
+                .AddLogging(c => c.AddSerilog())
+               .AddLogging(c => c.AddEventLog());
+
+          /*  services.AddSingleton<QRCode>()
+               .AddLogging(c => c.AddConsole())
+               .AddLogging(c => c.AddDebug())
+               .AddLogging(c => c.AddSerilog())
+               .AddLogging(c => c.AddEventLog()); */
+
+
+        }
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            var GameList = serviceProvider.GetService<GameList>();
+
+            //var screen = serviceProvider.GetService<QRCode>();
+
+            GameList.Show();
+        }
+
+
     }
 
 }

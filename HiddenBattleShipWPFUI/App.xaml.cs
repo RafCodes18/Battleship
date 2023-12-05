16250f014b3af38dec2a4bd4bbbcb3140107eab1
Kathy Xiong <700201382@fvtc.edu>
2023-12-04 23:13:57 -0600

    Changed the name for WPF

--- HiddenBattleShipWPFUI/App.xaml.cs
+++ HiddenBattleShipWPFUI/App.xaml.cs
@@ -2,7 +2,7 @@
 using System.Data;
 using System.Windows;
 
-namespace HiddenBattleShipWPFUI
+namespace HiddenBattleShip.WPFUI
 {
     /// <summary>
     /// Interaction logic for App.xaml
--- HiddenBattleShipWPFUI/MainWindow.xaml.cs
+++ HiddenBattleShipWPFUI/MainWindow.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace HiddenBattleShipWPFUI
+namespace HiddenBattleShip.WPFUI
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml

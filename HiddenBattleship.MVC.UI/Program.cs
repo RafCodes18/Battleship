c13e728fe76546a2dd8edcbc47aef35bada98d15
Peter Byers <500135802@fvtc.edu>
2023-12-12 06:46:41 -0600

    Uncommented Hub endpoints in program file for MVCUI

--- HiddenBattleship.MVC.UI/Program.cs
+++ HiddenBattleship.MVC.UI/Program.cs
@@ -40,11 +40,12 @@ if (!app.Environment.IsDevelopment())
 }
 
 //map the endpoints
-//app.UseEndpoints(endpoints =>
-//{
-//    endpoints.MapHub<ChatHub>("/chatHub");
-//    endpoints.MapHub<GameHub>("/gameHub");
-//});
+//TODO
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapHub<ChatHub>("/chatHub");
+endpoints.MapHub<GameHub>("/gameHub");
+});
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();

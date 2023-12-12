0da83d09c5f36c5ca684c85b2ee87c5a4136e3be
Peter Byers <500135802@fvtc.edu>
2023-12-12 06:19:17 -0600

    Finished Gets for entities in wpfui

--- HiddenBattleShip.WPFUI/GameList.xaml.cs
+++ HiddenBattleShip.WPFUI/GameList.xaml.cs
@@ -5,6 +5,7 @@ using PB.Utility;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,21 +24,25 @@ namespace HiddenBattleShip.WPFUI
     /// </summary>
     public partial class GameList : Window
     {
+
+        // list of entities
         List<Game> games = new List<Game>();
         List<Player> players = new List<Player>();
         List<ChatHistory> chatHistories = new List<ChatHistory>();
+        List<GameMoves> gameMoves = new List<GameMoves>();
+        List<Ship> ships = new List<Ship>();
+
         string selectedEntity;
 
         string APIAddress = "https://localhost:7270/api/";
         ApiClient apiClient;
-        int userInput;
+        
         private readonly ILogger<GameList> logger;
 
         public GameList()
         {
             InitializeComponent();
             apiClient = new ApiClient(APIAddress);
-            userInput = 0;
             string selectedEntity = "";
         }
 
@@ -46,7 +51,6 @@ namespace HiddenBattleShip.WPFUI
             InitializeComponent();
             apiClient = new ApiClient(APIAddress);
             this.logger = logger;
-            userInput = 0;
             string selectedEntity = "";
         }
 
@@ -72,6 +76,9 @@ namespace HiddenBattleShip.WPFUI
 
         private void btnGetChatHistory_Click(object sender, RoutedEventArgs e)
         {
+
+            selectedEntity = "ChatHistory";
+
             try
             {
 
@@ -92,6 +99,8 @@ namespace HiddenBattleShip.WPFUI
 
         private void btnGetPlayers_Click(object sender, RoutedEventArgs e)
         {
+            selectedEntity = "Player";
+
             try
             {
                 players = apiClient.GetList<Player>(typeof(Player).Name);
@@ -109,6 +118,49 @@ namespace HiddenBattleShip.WPFUI
             }
 
         }
+
+        private void btnGameMoves_Click(object sender, RoutedEventArgs e)
+        {
+            selectedEntity = "GameMove";
+            try
+            {
+                gameMoves = apiClient.GetList<GameMoves>(typeof(GameMoves).Name);
+
+                dgGames.ItemsSource = null;
+                dgGames.ItemsSource = gameMoves;
+
+
+                throw new Exception("Dependency Injection is cool. I have " + gameMoves.Count + " players.");
+
+            }
+            catch (Exception ex)
+            {
+                logger.LogWarning("Error: {UserId}", " ");
+            }
+
+        }
+
+        private void btnGetShips_Click(object sender, RoutedEventArgs e)
+        {
+            selectedEntity = "Ship";
+            try
+            {
+                ships = apiClient.GetList<Ship>(typeof(Ship).Name);
+
+                dgGames.ItemsSource = null;
+                dgGames.ItemsSource = ships;
+
+
+                throw new Exception("Dependency Injection is cool. I have " + ships.Count + " players.");
+
+            }
+            catch (Exception ex)
+            {
+                logger.LogWarning("Error: {UserId}", " ");
+            }
+        }
+
+
 
         private void btnUpdate_Click(object sender, RoutedEventArgs e)
         {
@@ -135,8 +187,53 @@ namespace HiddenBattleShip.WPFUI
                     dgGames.ItemsSource = games;
 
                 }
-            } else if (selectedEntity == )
-            
+            } 
+            else if (selectedEntity == "ChatHistory")
+            {
+                var selectedChatHistory = dgGames.SelectedItem as ChatHistory;
+                if (selectedChatHistory != null)
+                {
+
+                    var result = apiClient.Delete("ChatHistory", selectedChatHistory.Id);
+
+                    chatHistories = apiClient.GetList<ChatHistory>(typeof(ChatHistory).Name);
+                    dgGames.ItemsSource = null;
+                    dgGames.ItemsSource = chatHistories;
+                }
+            }
+            else if (selectedEntity == "Player")
+            {
+                var selectedPlayer = dgGames.SelectedItem as Player;
+                if (selectedPlayer != null)
+                {
+
+                    var result = apiClient.Delete("Player", selectedPlayer.Id);
+
+                    players = apiClient.GetList<Player>(typeof(Player).Name);
+                    dgGames.ItemsSource = null;
+                    dgGames.ItemsSource = players;
+                }
+            }
+            else if (selectedEntity == "GameMove")
+            {
+                var selectedGameMove = dgGames.SelectedItem as GameMoves;
+                if (selectedGameMove != null)
+                {
+
+                    var result = apiClient.Delete("GameMoves", selectedGameMove.MoveId);
+
+                    gameMoves = apiClient.GetList<GameMoves>(typeof(GameMoves).Name);
+                    dgGames.ItemsSource = null;
+                    dgGames.ItemsSource = gameMoves;
+                }
+            }
+            else if (selectedEntity == "Ship")
+            {
+
+            }
+
         }
+
+        
     }
 }
